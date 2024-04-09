3817e0981a46b56c71faf59989a84437b48cf90f
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-09 10:07:32 +0900

    [Add]fizzbuzz作成

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const fizzbuzz = (num) => {
+    if (num % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(String(num));
+    }
+};
+
+let num = 1;
+
+while (num <= 20) {
+    fizzbuzz(num);
+    num++;
+}
