6c1341e682301426237556d8b59e6bb7722b466f
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-11 05:43:30 +0900

    [Modify]fizzbuzz条件の変更
    15の倍数を3と5の倍数に変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 const fizzbuzz = (num) => {
-    if (num % 15 === 0) {
-        console.log("FizzBuzz");
-    } else if (num % 3 === 0) {
-        console.log("Fizz");
-    } else if (num % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(String(num));
-    }
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(String(num));
+  }
 };
 
 for (let i = 1; i <= 20; i++) {
