281ccfc7d319c044883933965ada5c19202f3db3
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-18 02:17:15 +0900

    コメント削除

--- 03.asynchronous/callback/callback.js
+++ 03.asynchronous/callback/callback.js
@@ -6,9 +6,8 @@ const CREATE_TABLE =
   "CREATE TABLE books (id integer primary key autoincrement, title text not null unique)";
 const INSERT_RECORD = "INSERT INTO books (title) VALUES(?)";
 const GET_ALL_RECORDS = "SELECT * FROM books";
-// テーブルを作成する
+
 db.run(CREATE_TABLE, () => {
-  //   2. レコードを追加し、自動採番された ID を標準出力に出力する
   db.run(INSERT_RECORD, ["book1"], function () {
     console.log(`id: ${this.lastID}のレコードが追加されました。 `);
     db.each(GET_ALL_RECORDS, (_, row) => {
