509a7febe83882cd3e62d2a23af9c734073e9d79
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-20 01:57:40 +0900

    [Modify]定数名変更
    SQLとわかるように

--- 03.asynchronous/callback/callback.js
+++ 03.asynchronous/callback/callback.js
@@ -2,15 +2,15 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const TABLE_CREATION_SQL =
+const SQL_CREATE_TABLE =
   "CREATE TABLE books (id integer primary key autoincrement, title text not null unique)";
-const RECORD_INSERTION_SQL = "INSERT INTO books (title) VALUES(?)";
-const ALL_RECORDS_ACQUISITION_SQL = "SELECT * FROM books";
+const SQL_INSERT_RECORD = "INSERT INTO books (title) VALUES(?)";
+const SQL_GET_ALL_RECORDS = "SELECT * FROM books";
 
-db.run(TABLE_CREATION_SQL, () => {
-  db.run(RECORD_INSERTION_SQL, ["book1"], function () {
+db.run(SQL_CREATE_TABLE, () => {
+  db.run(SQL_INSERT_RECORD, ["book1"], function () {
     console.log(`id: ${this.lastID}のレコードが追加されました。 `);
-    db.each(ALL_RECORDS_ACQUISITION_SQL, (_, row) => {
+    db.each(SQL_GET_ALL_RECORDS, (_, row) => {
       console.log(row);
       db.close();
     });
