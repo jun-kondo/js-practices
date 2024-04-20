ec048b50c2ee34aea48bd4ef97fb7dd143ec857f
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-20 13:17:02 +0900

    [Modify]標準エラー出力に変更

--- 03.asynchronous/callback/callback_error.js
+++ 03.asynchronous/callback/callback_error.js
@@ -10,11 +10,10 @@ const INVALID_SQL = "SELECT content FROM books";
 db.run(CREATE_TABLE, () => {
   db.run(INSERT_RECORD, ["book1"], () => {
     db.run(INSERT_RECORD, ["book1"], (err) => {
-      console.log(err.message);
+      console.error(err.message);
       db.each(INVALID_SQL, (err) => {
-        console.log(err.message);
+        console.error(err.message);
       });
     });
   });
 });
-db.close();
