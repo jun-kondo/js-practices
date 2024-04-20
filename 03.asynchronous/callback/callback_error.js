82668ca507a6f962c0702378832ce113f820bf1f
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-20 13:22:36 +0900

    [Add]テーブル削除処理

--- 03.asynchronous/callback/callback_error.js
+++ 03.asynchronous/callback/callback_error.js
@@ -6,6 +6,7 @@ const CREATE_TABLE =
   "CREATE TABLE books (id integer primary key autoincrement, title text not null unique)";
 const INSERT_RECORD = "INSERT INTO books (title) VALUES(?)";
 const INVALID_SQL = "SELECT content FROM books";
+const DROP_TABLE_QUERY = "DROP TABLE books";
 
 db.run(CREATE_TABLE, () => {
   db.run(INSERT_RECORD, ["book1"], () => {
@@ -13,6 +14,9 @@ db.run(CREATE_TABLE, () => {
       console.error(err.message);
       db.each(INVALID_SQL, (err) => {
         console.error(err.message);
+        db.run(DROP_TABLE_QUERY, () => {
+          db.close();
+        });
       });
     });
   });
