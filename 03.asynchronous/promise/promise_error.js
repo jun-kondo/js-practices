003a5871cc1d13a0e90a160129a8a1cd4f18395f
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-20 13:54:39 +0900

    [Modify]promise書き直し

--- 03.asynchronous/promise/promise_error.js
+++ 03.asynchronous/promise/promise_error.js
@@ -1,32 +1,36 @@
-import sqlite3 from "sqlite3";
-import {
-  createTable,
-  insertData,
-  getAllData,
-} from "../lib/asynchronous_functions.js";
+import { db, runAsync, eachAsync } from "../lib/asynchronous_functions.js";
 
-const db = new sqlite3.Database(":memory:");
-const tableName = "books";
-const data = "book1";
-const wrongColumn = "content";
+const CREATE_TABLE_QUERY =
+  "CREATE TABLE books (id integer primary key autoincrement, title text not null unique)";
+const INSERT_RECORD_QUERY = "INSERT INTO books (title) VALUES (?)";
+const INVALID_GET_ALL_RECORD_QUERY = "SELECT content FROM books";
+const DROP_TABLE_QUERY = "DROP TABLE books";
 
-createTable(db, tableName)
-  .then(() => {
-    return insertData(db, tableName, data);
-  })
-  .then(() => {
-    return insertData(db, tableName, data);
-  })
+runAsync(CREATE_TABLE_QUERY)
+  .then(() => runAsync(INSERT_RECORD_QUERY, ["book1"]))
+  .then(() => runAsync(INSERT_RECORD_QUERY, ["book1"]))
   .catch((err) => {
-    console.log(err.message);
+    if (
+      err &&
+      err.code === "SQLITE_CONSTRAINT" &&
+      err.message.includes("UNIQUE constraint failed")
+    ) {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
   })
-  .then(() => {
-    return getAllData(db, tableName, wrongColumn);
-  })
+  .then(() => eachAsync(INVALID_GET_ALL_RECORD_QUERY))
   .catch((err) => {
-    console.log(err.message);
+    if (
+      err &&
+      err.code === "SQLITE_ERROR" &&
+      err.message.includes("no such column")
+    ) {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
   })
-  .finally(() => {
-    db.close();
-    console.log("終了します");
-  });
+  .then(() => runAsync(DROP_TABLE_QUERY))
+  .finally(() => db.close());
