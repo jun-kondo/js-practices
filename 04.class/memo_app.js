2af3337b22123db85837f237e61f1c647e5987f9
jun-kondo <ranmaru.oo.q@gmail.com>
2024-07-11 13:55:28 +0900

    [delete]memo_crudクラス
    クラスの役割をmemo_appに移動、memo_crudクラスを削除

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 import { CommandOption } from "./command_option.js";
 import { Memo } from "./memo.js";
-import { MemoCrud } from "./memo_crud.js";
 import { MemoStorage } from "./memo_storage.js";
+import pkg from "enquirer";
+
+const { Select } = pkg;
 
 export class MemoApp {
   #commandOption;
   #dbManager;
-  #memoCrud;
+  // #memoCrud;
   constructor() {
     this.#commandOption = new CommandOption();
     this.#dbManager = new MemoStorage("memo_app.db");
-    this.#memoCrud = new MemoCrud(this.#dbManager);
   }
 
   async execute() {
@@ -28,14 +29,14 @@ export class MemoApp {
   async #run() {
     const options = this.#commandOption.parse();
     if (options.list) {
-      await this.#memoCrud.listMemos();
+      await this.#listMemos();
     } else if (options.read) {
-      await this.#memoCrud.readMemo();
+      await this.#readMemo();
     } else if (options.delete) {
-      await this.#memoCrud.deleteMemo();
+      await this.#deleteMemo();
     } else {
       const inputText = await this.#handleInput();
-      await this.#memoCrud.createMemo(new Memo(inputText));
+      await this.#createMemo(new Memo(inputText));
     }
   }
 
@@ -46,6 +47,85 @@ export class MemoApp {
       process.stdin.on("data", (chunk) => (text += chunk));
       process.stdin.on("end", () => resolve(text));
     });
+  }
+
+  async #createMemo(memo) {
+    try {
+      const result = await this.#dbManager.insertMemo(memo);
+      console.log(result);
+    } catch (e) {
+      throw new Error(`Error registering new memo: ${e.message}`);
+    }
+  }
+
+  async #listMemos() {
+    try {
+      const rows = await this.#dbManager.getAllMemos();
+      if (rows.length === 0) {
+        console.log("There are no registered memos.");
+      } else {
+        rows.forEach((row) => console.log(row.title));
+      }
+    } catch (e) {
+      throw new Error(`Error listing memos: ${e.message}`);
+    }
+  }
+
+  async #readMemo() {
+    const choices = await this.#getMemoChoices();
+    const message = "Choose a memo you want to read:";
+    const selectedChoice = await this.#selectMemo(choices, message);
+    if (selectedChoice) {
+      const memoId = selectedChoice.value;
+      try {
+        const result = await this.#dbManager.getMemoContent(memoId);
+        console.log(result);
+      } catch (e) {
+        throw new Error(`Error reading memo: ${e.message}`);
+      }
+    }
+  }
+
+  async #deleteMemo() {
+    const choices = await this.#getMemoChoices();
+    const message = "Choose a memo you want to delete:";
+    const selectedChoice = await this.#selectMemo(choices, message);
+    if (selectedChoice) {
+      const memoId = selectedChoice.value;
+      try {
+        const result = await this.#dbManager.deleteMemo(memoId);
+        console.log(result);
+      } catch (e) {
+        throw new Error(`Error deleting memo: ${e.message}`);
+      }
+    }
+  }
+
+  async #getMemoChoices() {
+    try {
+      const rows = await this.#dbManager.getAllMemos();
+      return rows.map((row) => ({
+        name: row.title,
+        value: row.id,
+      }));
+    } catch (e) {
+      throw new Error(`Error getting memo choices: ${e.message}`);
+    }
+  }
+
+  async #selectMemo(choices, message) {
+    if (choices.length === 0) {
+      console.log("There are no registered memos.");
+      return;
+    }
+    const prompt = new Select({
+      type: "select",
+      name: "title",
+      message: message,
+      choices: choices,
+    });
+    const answer = await prompt.run();
+    return prompt.choices.find((choice) => choice.name === answer);
   }
 }
 
